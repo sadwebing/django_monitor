6466e2c8ef3c90d9edbe30c46512367e8f52d757
arno <arno@ag866.com>
2017-03-01 12:59:58 +0800

    migrate

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django_monitor
+settings introdution:
+https://docs.djangoproject.com/en/1.10/ref/settings/
---
+++ check_tomcat/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class CheckTomcatConfig(AppConfig):
+    name = 'check_tomcat'
