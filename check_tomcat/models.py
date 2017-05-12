49ed6dfb5b492fedd72cd42c8927ddcca0b5a07d
Arno <Arno@ag866.com>
2017-05-12 16:23:00 +0800

    move user_profile to accounts

--- check_tomcat/models.py
+++ check_tomcat/models.py
@@ -42,21 +42,3 @@ class server_status(models.Model):
     url = models.CharField(max_length=128)
     status = models.CharField(max_length=10, null=True)
     info = models.CharField(max_length=1024, null=True)
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)    
-    role = models.CharField(max_length=200, default='', blank=True)
-    #address = models.CharField(max_length=200,default='',blank=True)
-
-    def __unicode__(self):
-        return self.user.username
-
-def create_user_profile(sender, instance, created, **kwargs):
-    """Create the UserProfile when a new User is saved"""
-    if created:
-        profile = UserProfile()
-        profile.user = instance
-        profile.save()
-
-#post_save.connect(create_user_profile, sender=User)
